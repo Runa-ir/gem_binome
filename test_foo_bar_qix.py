960a5e01b2bd4f4d876011d34008aee593d9c488
BR6067 <valentin.romanov@engie.com>
2020-10-16 10:10:16 +0200

    add two tests on division

--- test_foo_bar_qix.py
+++ test_foo_bar_qix.py
@@ -5,15 +5,30 @@ class FooBarQix(object):
 
     def generate(self, number):
 
-        return "1"
+        if number %3 == 0:
+            return "Foo"
+        if number %5 == 0:
+            return "Bar"
+        return str(number)
 
 
 class FooBarQixTest(unittest.TestCase):
-    def test_should_return_one_when_number_is_one(self):
+    def test_should_return_number_when_number(self):
 
         foo_bar_qix = FooBarQix()
-        actual = foo_bar_qix.generate(1)
-        self.assertEqual("1", actual)
+        actual = foo_bar_qix.generate(2)
+        self.assertEqual("2", actual)
+
+    def test_should_return_foo_when_divisible_by_three(self):
+
+        foo_bar_qix = FooBarQix()
+        actual = foo_bar_qix.generate(9)
+        self.assertEqual("Foo", actual)
+
+    def test_should_return_bar_when_divisible_by_five(self):
+        foo_bar_qix = FooBarQix()
+        actual = foo_bar_qix.generate(10)
+        self.assertEqual("Bar", actual)
 
 if __name__ == '__main__':
     unittest.main()
