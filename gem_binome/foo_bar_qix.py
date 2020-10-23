af37c11df0d09549d45f1aa2c2de849124e5216b
TF5378 <yannick.rabu@engie.com>
2020-10-23 10:02:46 +0200

    We now need functions

--- gem_binome/foo_bar_qix.py
+++ gem_binome/foo_bar_qix.py
@@ -5,7 +5,18 @@ class FooBarQix(object):
 
     def generate(self, number):
 
+        res = ''
         for k, v in self.map.items():
             if number % k == 0:
-                return v
+                res += v
+
+        for char in str(number):
+            for k, v in self.map.items():
+                if char == str(k):
+                    res += v
+
+        if res != '':
+            return res
+
         return str(number)
+
--- gem_binome/test_foo_bar_qix.py
+++ gem_binome/test_foo_bar_qix.py
@@ -16,8 +16,20 @@ class FooBarQixTest(unittest.TestCase):
     def test_should_return_qix_when_number_divisible_by_seven(self):
         self.assertEqual("Qix", self.foo_bar_qix.generate(14))
 
-    def test_should_return_foofoo_when_number_contains_three_and_is_divisible_by_three(self):
+    def test_should_return_foofoo_when_number_is_divisible_by_three_and_contains_three(self):
         self.assertEqual("FooFoo", self.foo_bar_qix.generate(3))
+
+    def test_should_return_barbar_when_number_is_divisible_by_five_and_contains_five(self):
+        self.assertEqual("BarBar", self.foo_bar_qix.generate(5))
+
+    def test_should_return_qixqix_when_number_is_divisible_by_seven_and_contains_seven(self):
+        self.assertEqual("QixQix", self.foo_bar_qix.generate(7))
+
+    def test_should_return_foofoofoo_when_number_is_divisible_by_three_and_contains_twice_three(self):
+        self.assertEqual("FooFooFoo", self.foo_bar_qix.generate(33))
+
+    def test_should_return_foobar_when_number_contains_three_and_five(self):
+        self.assertEqual("BarFoo", self.foo_bar_qix.generate(53))
 
     def setUp(self):
         self.foo_bar_qix = FooBarQix()
