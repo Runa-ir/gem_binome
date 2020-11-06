14715bad942db1d957419e4c0fde34009f1e6093
TF5378 <yannick.rabu@engie.com>
2020-11-06 09:36:44 +0100

    testing line break when size equals column number

--- word_wrapper/test_word_wrapper.py
+++ word_wrapper/test_word_wrapper.py
@@ -4,6 +4,8 @@ import unittest
 class WordWrapper(object):
 
     def wrap(self, words, column_number):
+        if len(words) > column_number:
+            return words[:column_number] + '\n' + words[column_number + 1:]
         return words
 
 
@@ -15,6 +17,13 @@ class TestsWordWrapper(unittest.TestCase):
         wrapper = WordWrapper()
         actual_string = wrapper.wrap(expected_string, column_number)
         self.assertEqual(expected_string, actual_string)
+
+    def test_should_break_at_the_boundary_when_column_number_matches_a_space_position_between_two_words(self):
+        input_string = "dummy string"
+        column_number = 5
+        wrapper = WordWrapper()
+        actual_string = wrapper.wrap(input_string, column_number)
+        self.assertEqual("dummy\nstring", actual_string)
 
 
 if __name__ == '__main__':
