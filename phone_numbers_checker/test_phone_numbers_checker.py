a7834c8af9ad9d787d8bb8c06affe731408ae1da
TF5378 <yannick.rabu@engie.com>
2021-01-15 09:29:53 +0100

    Work on whole phonebook instead of numbers list & order phone book by number to improve algorithm

--- phone_numbers_checker/test_phone_numbers_checker.py
+++ phone_numbers_checker/test_phone_numbers_checker.py
@@ -4,22 +4,30 @@ import unittest
 class PhoneNumberChecker:
 
     def __init__(self, phone_book: dict):
-        self.phone_book = phone_book
-        self.numbers = self._normalize_numbers(self._extract_numbers())
+        self.phone_book = self._normalize_phone_book(phone_book)
+        self.order_phone_book()
 
-    def _extract_numbers(self) -> list:
-        return list(self.phone_book.values())
+    def _normalize_phone_book(self, phone_book):
+        normalized = {}
+        for name, number in phone_book.items():
+            normalized[name] = "".join(filter(str.isalnum, number))
+        return normalized
 
-    def _normalize_numbers(self, numbers):
-        return ["".join(filter(str.isalnum, number)) for number in numbers]
+    def order_phone_book(self):
+        ordered = sorted(self.phone_book.items(), key=lambda item: item[1])
+        self.phone_book = {k: v for k, v in ordered}
 
     def check_consistency(self):
-        numbers_list = self.numbers.copy()
-        for n in self.numbers:
-            numbers_list.pop(0)
-            for m in numbers_list:
-                if m.startswith(n) or n.startswith(m):
-                    print(m, n)
+        phone_book = self.phone_book.copy()
+        for name, number in self.phone_book.items():
+            phone_book.pop(name)
+            if phone_book:
+                next_name = list(phone_book.keys())[0]
+                next_number = phone_book[next_name]
+
+                if next_number.startswith(number):
+                    print(name, number)
+                    print(next_name, next_number)
                     return False
 
         return True
@@ -44,6 +52,14 @@ class TestPhoneNumbers(unittest.TestCase):
         number_checker = PhoneNumberChecker(phone_book)
         self.assertEqual(False, number_checker.check_consistency())
 
+    def test_to_order_phone_book_by_numbers(self):
+        phone_book = {'Nick': "323", "Frank": "12", "Anna": "441"}
+        number_checker = PhoneNumberChecker(phone_book)
+        self.assertEqual({"Frank": "12", 'Nick': "323", "Anna": "441"}, number_checker.phone_book)
+
+        phone_book = {'Nick': "123", "Frank": "12"}
+        number_checker = PhoneNumberChecker(phone_book)
+        self.assertEqual({"Frank": "12", 'Nick': "123"}, number_checker.phone_book)
 
 
     def test_check_consistence_of_a_small_file(self):
@@ -58,8 +74,7 @@ class TestPhoneNumbers(unittest.TestCase):
                     continue
                 phone_book[name] = number
         number_checker = PhoneNumberChecker(phone_book)
-        self.assertEqual(True, number_checker.check_consistency())
-
+        self.assertEqual(False, number_checker.check_consistency())
 
 
 if __name__ == '__main__':
