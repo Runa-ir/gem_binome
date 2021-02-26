f369aa8237a02e200fe5f2526cd5e67c487f91d0
TF5378 <yannick.rabu@engie.com>
2021-02-26 09:34:14 +0100

    End of kata bowling (I guess ?)

--- kata_bowling/test_bowling_game.py
+++ kata_bowling/test_bowling_game.py
@@ -9,15 +9,18 @@ class BowlingGame(object):
         self.round_history = []
 
     def roll(self, pins):
-        self.points += pins
-
         pre_round = self.define_pre_round()
         pre_pre_round = self.define_pre_pre_round()
 
+        self.count_current_roll(pins, pre_round)
         self.deal_with_possible_strike(pins, pre_pre_round, pre_round)
         self.deal_with_possible_spare(pins, pre_round)
 
         self.update_round_history(pins)
+
+    def count_current_roll(self, pins, pre_round):
+        if len(self.round_history) < 10 or (pre_round and sum(pre_round) < 10):
+            self.points += pins
 
     def define_pre_round(self) -> Optional[Tuple]:
         if len(self.round_history) > 0:
@@ -36,6 +39,9 @@ class BowlingGame(object):
         if pre_round:
             if len(pre_round) == 1 and pre_round[0] == 10:
                 self.points += pins
+            # Last round strike case
+            if len(self.round_history) == 10 and len(pre_round) == 2 and pre_round[0] == 10:
+                self.points += pins
 
     def deal_with_possible_spare(self, pins, pre_round):
         if pre_round:
@@ -44,7 +50,8 @@ class BowlingGame(object):
 
     def update_round_history(self, pins):
         new_round_criteria = (
-                    not self.round_history or sum(self.round_history[-1]) == 10 or len(self.round_history[-1]) == 2
+            (not self.round_history or sum(self.round_history[-1]) == 10 or len(self.round_history[-1]) == 2)
+            and len(self.round_history) < 10
         )
         if new_round_criteria:
             self.round_history.append((pins,))
@@ -95,9 +102,9 @@ class TestBowlingGame(unittest.TestCase):
         self.assertEqual(22, bowling_game.score())
 
     def test_strike_result(self):
-        # pins = [10, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        # bowling_game = self.play_bowling_game(pins)
-        # self.assertEqual(18, bowling_game.score())
+        pins = [10, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        bowling_game = self.play_bowling_game(pins)
+        self.assertEqual(18, bowling_game.score())
 
         pins = [10, 4, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         bowling_game = self.play_bowling_game(pins)
@@ -111,20 +118,29 @@ class TestBowlingGame(unittest.TestCase):
         bowling_game = self.play_bowling_game(pins)
         self.assertEqual(46, bowling_game.score())
 
+    def test_last_round_cases(self):
+        pins = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 3, 6]
+        bowling_game = self.play_bowling_game(pins)
+        self.assertEqual(19, bowling_game.score())
+
+        pins = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 4, 6]
+        bowling_game = self.play_bowling_game(pins)
+        self.assertEqual(16, bowling_game.score())
+
     def test_combination_of_strikes_and_spares(self):
         pins = [10, 5, 5, 10, 5, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         bowling_game = self.play_bowling_game(pins)
         self.assertEqual(64, bowling_game.score())
 
     def test_update_round_history(self):
-        pins = [10, 5, 5, 10, 5, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        pins = [10, 5, 5, 10, 5, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         bowling_game = self.play_bowling_game(pins)
-        expected_round_history = [(10,), (5, 5), (10,), (5, 2), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0)]
+        expected_round_history = [(10,), (5, 5), (10,), (5, 2), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0)]
         self.assertEqual(expected_round_history, bowling_game.round_history)
 
-        pins = [0, 10, 5, 5, 10, 5, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        pins = [0, 10, 5, 5, 10, 5, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         bowling_game = self.play_bowling_game(pins)
-        expected_round_history = [(0, 10), (5, 5), (10,), (5, 2), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0)]
+        expected_round_history = [(0, 10), (5, 5), (10,), (5, 2), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0)]
         self.assertEqual(expected_round_history, bowling_game.round_history)
 
 
