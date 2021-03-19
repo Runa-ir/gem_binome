e5fe86437f713fac93b0b93fee4738a4d0cf818a
BR6067 <valentin.romanov@engie.com>
2021-03-19 10:14:49 +0100

    end of the prime numbers

--- prime_factors/test_prime_factors.py
+++ prime_factors/test_prime_factors.py
@@ -3,37 +3,32 @@ import unittest
 
 class PrimeFactor(object):
 
+    factors = []
+
     def generate(self, input_number):
-        factors = []
         quotient = input_number
         derivative = 2
 
+        quotient = self.extract_factor_and_change_quotient(derivative, quotient)
+
+        derivative = 3
+        while quotient >= derivative:
+            quotient = self.extract_factor_and_change_quotient(derivative, quotient)
+            derivative += 2
+
+            # if derivative % 3 == 0 and derivative > 3:
+            #     derivative += 2
+
+        if not self.factors:
+            self.factors.append(quotient)
+
+        return self.factors
+
+    def extract_factor_and_change_quotient(self, derivative, quotient):
         while quotient % derivative == 0:
-            factors.append(derivative)
+            self.factors.append(derivative)
             quotient = quotient / derivative
-
-        if input_number == 71398778977:
-            derivative = 3
-            while quotient >= derivative:
-                while quotient % derivative == 0:
-                    factors.append(derivative)
-                    quotient = quotient / derivative
-                derivative += 2
-
-                if derivative % 3 == 0 and derivative > 3:
-                    derivative += 2
-        if input_number == 713987787:
-            derivative = 3
-            while quotient >= derivative:
-                while quotient % derivative == 0:
-                    factors.append(derivative)
-                    quotient = quotient / derivative
-                derivative += 2
-
-        if not factors:
-            factors.append(quotient)
-
-        return factors
+        return quotient
 
 
 class TestPrimeFactors(unittest.TestCase):
@@ -51,15 +46,17 @@ class TestPrimeFactors(unittest.TestCase):
 
     def test_returns_list_of_derivatives_if_any(self):
         prime_factor = PrimeFactor()
-        #self.assertEqual([2, 2], prime_factor.generate(4))
+        self.assertEqual([2, 2], prime_factor.generate(4))
 
-        #self.assertEqual([2, 17], prime_factor.generate(34))
+        self.assertEqual([2, 17], prime_factor.generate(34))
 
-        #self.assertEqual([3, 3, 5, 7], prime_factor.generate(315))
+        self.assertEqual([3, 3, 5, 7], prime_factor.generate(315))
 
-        #self.assertEqual([2707, 26375611], prime_factor.generate(71398778977))
+        self.assertEqual([2707, 26375611], prime_factor.generate(71398778977))
 
-        self.assertEqual([2707, 26375611], prime_factor.generate(713987787))
+        self.assertEqual([3, 3, 3, 5, 5, 7, 31, 37], prime_factor.generate(5419575))
+
+        self.assertEqual([3, 237995929], prime_factor.generate(713987787))
 
 
 if __name__ == '__main__':
